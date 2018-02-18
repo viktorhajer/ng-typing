5f359b8a14bae0583491e15c7e63b5433bf193c7
viktorhajer <hajer.viktor@gmail.com>
2018-02-18 14:00:55 +0100

    Updated package generation

--- libs/ng-typing/index.ts
+++
@@ -1,2 +0,0 @@
-export { TypingModule } from './src/typing.module';
-export { TypingComponent } from './src/typing.component';
---
+++ public_api.ts
@@ -0,0 +1,2 @@
+export { TypingModule } from './libs/ng-typing/src/typing.module';
+export { TypingComponent } from './libs/ng-typing/src/typing.component';
